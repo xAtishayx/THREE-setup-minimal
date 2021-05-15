afdc7e9b3342f7b10cdad427c53a2540dbd30572
xatishayx <atishay.srivastava.10@gmail.com>
2021-05-15 15:29:33 +0530

    template ready

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from "./node_modules/three/build/three.module.js";
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 
 export default class sketch {
